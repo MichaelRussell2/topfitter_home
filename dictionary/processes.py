c120a97ffd6841237bbb17ca87645845a189c228
MichaelRussell <m.russell.2@research.gla.ac.uk>
2016-09-14 14:52:39 +0100

    Dictionary for processes, new datasets

---
+++ dictionary/processes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2.7
+
+# these should be clear enough, but just in case
+dictionary = {
+
+    #Unstable particles not decayed
+    "ttbar:  ": "Top pair production, with tops undecayed",
+    "tchan:  ": "t-channel single top production",
+    "schan:  ": "s-channel single top production",
+    "tW:     ": "single top production in association with a W boson",
+    "ttZ:    ": "Top pair production in association with a Z boson",
+    "ttH:    ": "Top pair production in association with a Higgs boson",
+    "ttW:    ": "Top pair production in association with a W boson",
+    "ttgam:  ": "Top pair production in association with a photon",
+    "topdk:  ": "Top decay width into W and b (assume Vtb = 1)",
+
+    #Unstable particles decayed
+    "ttbar_s:": "Top pair production, in semileptonic decay channel",
+    "ttbar_h:": "Top pair production, in fully hadronic decay channel",
+    "topdk_h:": "Top decay width into hadrons",
+    "topdk_l:": "Top decay width into leptons",
+}
