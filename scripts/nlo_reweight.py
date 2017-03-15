2b961d358931340e9c98ed68438ce72fc7219daa
Michael Russell <m.russell.2@research.gla.ac.uk>
2017-03-15 18:25:16 +0000

    run_scan and nlo_reweight broken into functions

--- scripts/nlo_reweight.py
+++ scripts/nlo_reweight.py
@@ -9,44 +9,53 @@ Output files xlo xhi y dy
 """
 
 import sys, os, shutil, glob
-
-import optparse
-op = optparse.OptionParser()
-opts, args = op.parse_args()
+import numpy as np
 
 indir="results"
 kdir="kfactors"
 outdir="results_reweighted"
-shutil.rmtree(outdir,ignore_errors=True)
-os.mkdir(outdir)
 
-dsize = len(os.walk(indir).next()[1])
-import numpy as np
-for i in xrange(dsize):
-    infiles = glob.glob(os.path.join(indir,"%03d" %i ,'*.dat'))
-    os.mkdir(os.path.join(outdir,"%03d" %i))
-    shutil.copy(os.path.join(indir,"%03d" %i,"used_params"),os.path.join(outdir,"%03d" %i))
+def main():
+    shutil.rmtree(outdir,ignore_errors=True)
+    os.mkdir(outdir)
 
-    for infile in infiles:
-        if np.loadtxt(infile,usecols=None).shape[0] > 2:
-            x_min, x_max, y_lo  = np.loadtxt(infile,usecols=(0,1,2),unpack=True)
+    dsize = len(os.walk(indir).next()[1])
+    for i in xrange(dsize):
+        infiles = glob.glob(os.path.join(indir,"%03d" %i ,'*.dat'))
+        os.mkdir(os.path.join(outdir,"%03d" %i))
+        shutil.copy(os.path.join(indir,"%03d" %i,"used_params"),os.path.join(outdir,"%03d" %i))
 
-            kfile=outfile=infile.split('/')[-1]
-            ks=np.loadtxt(os.path.join(kdir,kfile),usecols=None)
-            ks_nrows, ks_ncols = ks.shape[0], ks.shape[1]
+        for infile in infiles:
+            fname = os.path.basename(infile)
+            outfile = os.path.join(outdir,"%03d" %i,fname)
+            nlo_rw(infile,outfile)
 
-            assert len(y_lo) == ks_nrows 
-            if ks_ncols == 3:
-                y_nlo=y_lo*ks[:,1]
-                y_nlo_err=y_nlo*ks[:,2]
-            elif ks_ncols == 4:
-                y_nlo=y_lo*ks[:,2]
-                y_nlo_err=y_nlo*ks[:,3]
-            np.savetxt(os.path.join(outdir,"%03d" %i, outfile),np.c_[x_min,x_max,y_nlo,y_nlo_err], fmt="%.5f")
-        else:
-            y_lo, dy_lo  = np.loadtxt(infile,usecols=(0,1),unpack=True)
-            kfile=outfile=infile.split('/')[-1]
-            ks=np.loadtxt(os.path.join(kdir,kfile),usecols=None)
-            y_nlo=y_lo*ks[0]
-            y_nlo_err=y_nlo*ks[1]
-            np.savetxt(os.path.join(outdir,"%03d" %i, outfile),np.c_[y_nlo,y_nlo_err], fmt="%.5f")
+def nlo_rw(histo,outfile):
+
+    if np.loadtxt(histo,usecols=None).shape[0] > 2:
+        x_min, x_max, y_lo  = np.loadtxt(histo,usecols=(0,1,2),unpack=True)
+
+        kfile=os.path.join(kdir,os.path.basename(histo))
+        ks=np.loadtxt(kfile,usecols=None)
+        ks_nrows, ks_ncols = ks.shape[0], ks.shape[1]
+
+        assert len(y_lo) == ks_nrows 
+        if ks_ncols == 3:
+            y_nlo=y_lo*ks[:,1]
+            y_nlo_err=y_nlo*ks[:,2]
+        elif ks_ncols == 4:
+            y_nlo=y_lo*ks[:,2]
+            y_nlo_err=y_nlo*ks[:,3]
+        np.savetxt(outfile,np.c_[x_min,x_max,y_nlo,y_nlo_err], fmt="%.5f")
+
+    elif np.loadtxt(histo,usecols=None).shape[0] == 2:
+        y_lo, dy_lo  = np.loadtxt(histo,usecols=(0,1),unpack=True)
+        kfile=os.path.join(kdir,os.path.basename(histo))
+        ks=np.loadtxt(kfile,usecols=None)
+        y_nlo=y_lo*ks[0]
+        y_nlo_err=y_nlo*ks[1]
+
+        np.savetxt(outfile,np.c_[y_nlo,y_nlo_err], fmt="%.5f")
+
+if __name__ == "__main__":
+    main()
--- scripts/run_scan.py
+++ scripts/run_scan.py
@@ -2,7 +2,12 @@
 """\
 %prog [opts]
 
-Start running MadEvent samples in series (see make_batch_grid.py for parallel options) 
+Start running MadEvent samples in series (see make_batch_grid.py for parallel options)
+Requires as input:
+ * 'param_space' directory with numbered subdirectories and used_params file in each subdir
+ * 'samples' directory with MadEvent stuff: bin, source, subprocesses etc.
+ * 'param_card_sm.dat' : default (SM) parameter card for MadEvent
+ * if analysing events with FastPartons, subdir of samples called 'Analysis' with FastPartons src
 """
 
 import os, shutil, re, fileinput, glob
@@ -19,133 +24,148 @@ op.add_option("--info",dest="INFO", default=False, action="store_true", help="Pr
 
 opts, args = op.parse_args()
 
-main=os.getcwd()
+maindir=os.getcwd()
 
 ## make results directories
 dirs=["results","samples/outputs","samples/Events"]
 for d in dirs:
     shutil.rmtree(d,ignore_errors=True)
     os.mkdir(d)
-outdir=os.path.join(main,"results")
-    
+outdir=os.path.join(maindir,"results")
 
 ## get number of parameter space points
 size=opts.NSCAN 
-param_space = 'param_space_lin'
+param_space = 'param_space'
 dsize = len(os.walk(param_space).next()[1])
 if size > dsize:
     print "Number of requested points exceeds number of available param space points. Exiting"
     exit(1)
 
 ## operators in model parameter card to scan
+#ops=["ReCuW33","ReCuB33","ReC3Phiq33","ReC1Phiq33","ReCPhiu33"]
 ops=["CG","ReCuG33","C1qq1331","C8qu3311","C3qq1331","C8ud3311"]
-## ops=["ReCuB33","ReCuW33","ReCPhiu33","ReC1Phiq33","ReC3Phiq33"]
 
-## start running MadEvent samples
-for i in xrange(size):
-    dname = "results/%03d" % i
-    os.mkdir(dname)
+def main():
+
+    ## start running MadEvent samples
+    for i in xrange(size):
+        dname = "results/%03d" % i
+        os.mkdir(dname)
+        shutil.copy(os.path.join(param_space,'%03d' % i, 'used_params'),dname)
+    
+        ## write parameter space point to list
+        cs = []
+        for ind, line in enumerate(open(param_space+'/%03d/used_params' % i )):
+            cs.append(line.split()[1])
+            cs[ind] = float(cs[ind])
+        
+        print 'At run %03d' % i
+        banner = 'samples/outputs/' + 'output%03d' %i 
+
+        if i==1:
+            import datetime
+            start=datetime.datetime.now()
+
+        ## generate samples at param space point
+        run_point(cs,banner)
+        extract_xsec(banner,os.path.join(maindir,dname,'xsec.dat'))
+
+        ## (optionally) analyse events using FastPartons
+        evts = os.path.join('samples','Events','run_01','unweighted_events.lhe.gz')
+        if not opts.XSEC:
+            analyse_point(i, evts, os.path.abspath(dname))
+
+        shutil.rmtree('samples/Events/run_01')
+
+        if i==1:
+            elapsed=datetime.datetime.now()
+            totdur = (opts.NSCAN-1)*((elapsed-start))
+            print "%d runs requested. Estimated finish time:" % opts.NSCAN
+            print (start+totdur).ctime()
+            print
+
+def run_point(PARAMPT,runlog):
     f = os.path.join('samples','Cards','param_card.dat')
     shutil.copy('param_card_sm.dat', f)
-    shutil.copy(os.path.join(param_space,'%03d' % i, 'used_params'),dname)
-    
-    ## match operators to parameter space point
-    cs = []
-    for ind, line in enumerate(open(param_space+'/%03d/used_params' % i )):
-        cs.append(line.split()[1])
-        cs[ind] = float(cs[ind])
+
+    ## check dimensionality
     try:
-        assert len(cs)==len(ops)        
+        assert len(PARAMPT)==len(ops)        
     except:
         stderr.write("Number of operators must match dimensionality of parameter space. Exiting\n")
         exit(1)
 
-
+    ## replace SM param_card.dat with values of operators 
     for line in fileinput.FileInput(f,inplace=1):
         for j in xrange(len(ops)):
-            line = line.replace("0e-08 # %s " %ops[j], "%fe-08 # %s " % (cs[j], ops[j]))
+            line = line.replace("0e-08 # %s " % ops[j], "%fe-08 # %s " % (PARAMPT[j], ops[j]))
         print line,
     for line in fileinput.FileInput(os.path.join('samples','Cards','run_card.dat'),inplace=1):
-        line = line.replace("100000 = nevents", "%d = nevents" %opts.NEVTS )
+        line = line.replace("100000 = nevents", "%d = nevents" % opts.NEVTS )
         print line,
 
-    if i == 1:
-        import datetime
-        start=datetime.datetime.now()
-        
-
-    ## generate events
-    os.chdir('samples')
-    g=open('outputs/' + 'output%03d' %i , 'w' )
-    print 'At run %03d' % i
-    if opts.INFO:
-        print "Wilson coefficients:"
-        for j in xrange(len(ops)):
-            print "%s = %f [TeV^-2]" % (ops[j], cs[j]/100)
-        print
+    ## generate events, redirect MadEvent output 
     print 'Generating events'
-    call(["./bin/generate_events","-f"], stdout=g )
+    outstream = open(runlog, 'w')
+    call(["./samples/bin/generate_events","-f"], stdout=outstream )
+    outstream.close()
     print 'Done'
+
+
+def extract_xsec(runlog,outfile):
+
+    ## read cross-section from MadEvent output
+    for line in open(runlog,"r"):
+        if re.match("(.*)Cross-section(.*)", line):
+            xsec = float(line.split()[2])
+            xsec_err = float(line.split()[4]) 
+
+            ## write cross-section to file           
+            with open(outfile, 'w') as fout:
+                fout.write('%g  %g\n' % (xsec, xsec_err))
+                fout.close()
+
+def analyse_point(i,evtfile,outdir):
     
-    ## optionally plot events with FastPartons
-    if not opts.XSEC:
-        evtfile=os.path.join('Events','run_01','unweighted_events.lhe.gz')
-        if os.path.exists(evtfile):
-            shutil.move(evtfile,'Analysis')
-            os.chdir('Analysis')
+    shutil.move(evtfile,'samples/Analysis')
+    os.chdir('samples/Analysis')
+    
+    ## compile analysis on first run
+    if i==0:
+        print "Building FastPartons"
+        try:
+            assert(call(["make"],stdout=open(os.devnull, 'wb')) == 0)
+            print "Done"
+        except:
+            stderr.write("Failed to build FastPartons. Exiting\n")
+            exit(1)
 
-            ## compile analysis on first run
-            if i==0:
-                print "Building FastPartons"
-                try:
-                    assert(call(["make"],stdout=open(os.devnull, 'wb')) == 0)
-                    print "Done"
-                except:
-                    stderr.write("Failed to build FastPartons. Exiting\n")
-                    exit(1)
+    ## plot events and delete after plotting
+    call(["gunzip","unweighted_events.lhe.gz"])
+    call(["./analysis", "unweighted_events.lhe" ])
+    os.remove("unweighted_events.lhe")
+    infiles = glob.glob("*.dat")
 
-            ## plot events and delete after plotting
-            call(["gunzip","unweighted_events.lhe.gz"])
-            call(["./analysis", "unweighted_events.lhe" ])
-            os.remove("unweighted_events.lhe")
-            infiles = glob.glob("*.dat")
+    ## check binning is same as kfactors
+    if i==0:
+        import numpy as np
+        for infile in infiles:
+            npinfile = np.loadtxt(infile)
+            try:
+                kfile = np.loadtxt(os.path.join(maindir,"kfactors",infile))
+                assert len(npinfile) == len(kfile)
+                ans='y'
+            except AssertionError:
+                ans=raw_input("Warning: k-factor binning mismatch for %s. Continue anyway? (y/n) " % infile)
+            except IOError:
+                ans=raw_input("Warning: no k-factors for %s. Continue anyway? (y/n) " % infile)
+            if ans.lower()== ('y'):
+                continue
+            else: exit(1)
 
-            ## check binning is same as kfactors
-            if i==0:
-                import numpy as np
-                
-                for infile in infiles:
-                    npinfile = np.loadtxt(infile)
-                    try:
-                        kfile = np.loadtxt(os.path.join(main,"kfactors",infile))
-                        assert len(npinfile) == len(kfile)
-                        ans='y'
-                    except AssertionError:
-                        ans=raw_input("Warning: k-factor binning mismatch for %s. Continue anyway? (y/n) " % infile)
-                    except IOError:
-                        ans=raw_input("Warning: no k-factors for %s. Continue anyway? (y/n) " % infile)
-                    if ans.lower()== ('y'):
-                        continue
-                    else: exit(1)
-            [shutil.copy(i,os.path.join(main,dname)) for i in infiles]
-        else:
-            print 'Events were not generated for run %03d' % i
-    else:
-        f = 'outputs/' + 'output%03d' %i
-        for line in open(f,"r"):
-            if re.match("(.*)Cross-section(.*)", line):
-                xsec = float(line.split()[2])
-                xsec_err = float(line.split()[4])
-                with open(os.path.join(main,dname,'xsec.dat'), 'w') as fout:
-                    fout.write('%g  %g\n' % (xsec, xsec_err))
-                    fout.close()
+    ## copy histograms over to results directory
+    [shutil.copy(f,outdir) for f in infiles]
+    os.chdir(maindir)
 
-    if i==1:
-        elapsed=datetime.datetime.now()
-        totdur = (opts.NSCAN-1)*((elapsed-start))
-        print "%d runs requested. Estimated finish time:" % opts.NSCAN
-        print (start+totdur).ctime()
-        print
-
-    os.chdir(main)
-    shutil.rmtree('samples/Events/run_01')
+if __name__ == "__main__":
+    main()
